3397cc88e702db56ac76edeabcd21575acdeccf3
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-04-29 16:55:52 +0700

    menambahkan library cart

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('session', 'form_validation', 'upload', 'database', 'user_login');
+$autoload['libraries'] = array('session', 'form_validation', 'upload', 'database', 'cart', 'user_login');
 
 /*
 | -------------------------------------------------------------------
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array();
+$autoload['model'] = array('m_home');
