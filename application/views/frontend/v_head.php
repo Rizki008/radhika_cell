b589c7138d220c1730486d6f104abff59121b5b5
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-04-29 16:57:07 +0700

    menambahkan view frontend

---
+++ application/views/frontend/v_head.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Home | E-Shopper</title>
+    <link href="<?= base_url() ?>eshopper/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= base_url() ?>eshopper/css/font-awesome.min.css" rel="stylesheet">
+    <link href="<?= base_url() ?>eshopper/css/prettyPhoto.css" rel="stylesheet">
+    <link href="<?= base_url() ?>eshopper/css/price-range.css" rel="stylesheet">
+    <link href="<?= base_url() ?>eshopper/css/animate.css" rel="stylesheet">
+    <link href="<?= base_url() ?>eshopper/css/main.css" rel="stylesheet">
+    <link href="<?= base_url() ?>eshopper/css/responsive.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+    <link rel="shortcut icon" href="<?= base_url() ?>eshopper/images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?= base_url() ?>eshopper/images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?= base_url() ?>eshopper/images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?= base_url() ?>eshopper/images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="<?= base_url() ?>eshopper/images/ico/apple-touch-icon-57-precomposed.png">
+</head>
+<!--/head-->
