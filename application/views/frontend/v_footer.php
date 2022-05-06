99171bbda4cc9443f27717cbd7f7bba3a9857cea
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-05-07 05:23:30 +0700

    menambahkan proses javascript

--- application/views/frontend/v_footer.php
+++ application/views/frontend/v_footer.php
@@ -14,7 +14,7 @@
                         <div class="video-gallery text-center">
                             <a href="#">
                                 <div class="iframe-img">
-                                    <img src="images/home/iframe1.png" alt="" />
+                                    <img src="<?= base_url() ?>eshopper/images/home/iframe1.png" alt="" />
                                 </div>
                                 <div class="overlay-icon">
                                     <i class="fa fa-play-circle-o"></i>
@@ -29,7 +29,7 @@
                         <div class="video-gallery text-center">
                             <a href="#">
                                 <div class="iframe-img">
-                                    <img src="images/home/iframe2.png" alt="" />
+                                    <img src="<?= base_url() ?>eshopper/images/home/iframe2.png" alt="" />
                                 </div>
                                 <div class="overlay-icon">
                                     <i class="fa fa-play-circle-o"></i>
@@ -44,7 +44,7 @@
                         <div class="video-gallery text-center">
                             <a href="#">
                                 <div class="iframe-img">
-                                    <img src="images/home/iframe3.png" alt="" />
+                                    <img src="<?= base_url() ?>eshopper/images/home/iframe3.png" alt="" />
                                 </div>
                                 <div class="overlay-icon">
                                     <i class="fa fa-play-circle-o"></i>
@@ -59,7 +59,7 @@
                         <div class="video-gallery text-center">
                             <a href="#">
                                 <div class="iframe-img">
-                                    <img src="images/home/iframe4.png" alt="" />
+                                    <img src="<?= base_url() ?>eshopper/images/home/iframe4.png" alt="" />
                                 </div>
                                 <div class="overlay-icon">
                                     <i class="fa fa-play-circle-o"></i>
@@ -72,7 +72,7 @@
                 </div>
                 <div class="col-sm-3">
                     <div class="address">
-                        <img src="images/home/map.png" alt="" />
+                        <img src="<?= base_url() ?>eshopper/images/home/map.png" alt="" />
                         <p>505 S Atlantic Ave Virginia Beach, VA(Virginia)</p>
                     </div>
                 </div>
@@ -166,6 +166,7 @@
 <script src="<?= base_url() ?>eshopper/js/price-range.js"></script>
 <script src="<?= base_url() ?>eshopper/js/jquery.prettyPhoto.js"></script>
 <script src="<?= base_url() ?>eshopper/js/main.js"></script>
+
 </body>
 
 </html>
