75a0d7b2e84d9877eb2cea715ffa4c32099b09bb
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-05-08 12:12:31 +0700

    menambahkan data tables

--- application/views/backend/v_footer.php
+++ application/views/backend/v_footer.php
@@ -8,12 +8,23 @@
 <script src="<?= base_url() ?>deskapp-master/vendors/scripts/script.min.js"></script>
 <script src="<?= base_url() ?>deskapp-master/vendors/scripts/process.js"></script>
 <script src="<?= base_url() ?>deskapp-master/vendors/scripts/layout-settings.js"></script>
-<script src="<?= base_url() ?>deskapp-master/src/plugins/apexcharts/apexcharts.min.js"></script>
+<!-- <script src="<?= base_url() ?>deskapp-master/src/plugins/apexcharts/apexcharts.min.js"></script> -->
 <script src="<?= base_url() ?>deskapp-master/src/plugins/datatables/js/jquery.dataTables.min.js"></script>
 <script src="<?= base_url() ?>deskapp-master/src/plugins/datatables/js/dataTables.bootstrap4.min.js"></script>
 <script src="<?= base_url() ?>deskapp-master/src/plugins/datatables/js/dataTables.responsive.min.js"></script>
 <script src="<?= base_url() ?>deskapp-master/src/plugins/datatables/js/responsive.bootstrap4.min.js"></script>
 <script src="<?= base_url() ?>deskapp-master/vendors/scripts/dashboard.js"></script>
+<!-- buttons for Export datatable -->
+<script src="<?= base_url() ?>deskapp-master/src/plugins/datatables/js/dataTables.buttons.min.js"></script>
+<script src="<?= base_url() ?>deskapp-master/src/plugins/datatables/js/buttons.bootstrap4.min.js"></script>
+<script src="<?= base_url() ?>deskapp-master/src/plugins/datatables/js/buttons.print.min.js"></script>
+<script src="<?= base_url() ?>deskapp-master/src/plugins/datatables/js/buttons.html5.min.js"></script>
+<script src="<?= base_url() ?>deskapp-master/src/plugins/datatables/js/buttons.flash.min.js"></script>
+<script src="<?= base_url() ?>deskapp-master/src/plugins/datatables/js/pdfmake.min.js"></script>
+<script src="<?= base_url() ?>deskapp-master/src/plugins/datatables/js/vfs_fonts.js"></script>
+<!-- Datatable Setting js -->
+<script src="<?= base_url() ?>deskapp-master/vendors/scripts/datatable-setting.js"></script>
+</body>
 
 <script>
     function bacaGambar(input) {
