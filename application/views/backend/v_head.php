6a293d799181dcf127cdc77384fe2d3cad809fb8
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-05-08 20:57:24 +0700

    mengubah nama log

--- application/views/backend/v_head.php
+++ application/views/backend/v_head.php
@@ -4,7 +4,7 @@
 <head>
     <!-- Basic Page Info -->
     <meta charset="utf-8">
-    <title>DeskApp - Bootstrap Admin Dashboard HTML Template</title>
+    <title><?= $title ?></title>
 
     <!-- Site favicon -->
     <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url() ?>deskapp-master/vendors/images/apple-touch-icon.png">
