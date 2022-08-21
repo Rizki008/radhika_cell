fe4b88ccac2ac81d3fd68790b8a84015fe281c49
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-08-21 20:48:32 +0700

    menambahkan view pemilik

---
+++ application/views/v_pemilik.php
@@ -0,0 +1,262 @@
+<div class="main-container">
+    <div class="pd-ltr-20">
+        <div class="card-box pd-20 height-100-p mb-30">
+            <div class="row align-items-center">
+                <div class="col-md-4">
+                    <img src="<?= base_url() ?>deskapp-master/vendors/images/banner-img.png" alt="">
+                </div>
+                <div class="col-md-8">
+                    <h4 class="font-20 weight-500 mb-10 text-capitalize">
+                        Welcome back <div class="weight-600 font-30 text-blue">Pemilik</div>
+                    </h4>
+                    <p class="font-18 max-width-600">Radhika Celluler</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xl-3 mb-30">
+                <div class="card-box height-100-p widget-style1">
+                    <div class="d-flex flex-wrap align-items-center">
+                        <div class="progress-data">
+                            <!-- <div id="chart"></div> -->
+                        </div>
+                        <div class="widget-data">
+                            <div class="h4 mb-0">1</div>
+                            <div class="weight-600 font-14">pelanggan</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 mb-30">
+                <div class="card-box height-100-p widget-style1">
+                    <div class="d-flex flex-wrap align-items-center">
+                        <div class="progress-data">
+                            <!-- <div id="chart2"></div> -->
+                        </div>
+                        <div class="widget-data">
+                            <div class="h4 mb-0">5</div>
+                            <div class="weight-600 font-14">Produk</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 mb-30">
+                <div class="card-box height-100-p widget-style1">
+                    <div class="d-flex flex-wrap align-items-center">
+                        <div class="progress-data">
+                            <!-- <div id="chart3"></div> -->
+                        </div>
+                        <div class="widget-data">
+                            <div class="h4 mb-0">2</div>
+                            <div class="weight-600 font-14">Transaksi</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 mb-30">
+                <div class="card-box height-100-p widget-style1">
+                    <div class="d-flex flex-wrap align-items-center">
+                        <div class="progress-data">
+                            <!-- <div id="chart4"></div> -->
+                        </div>
+                        <div class="widget-data">
+                            <div class="h4 mb-0">Rp.10.000.000</div>
+                            <div class="weight-600 font-14">Pendapatan</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- <div class="row"> -->
+        <!-- <div class="col-xl-8 mb-30">
+                <div class="card-box height-100-p pd-20">
+                    <h2 class="h4 mb-20">Activity</h2>
+                    <div id="chart5"></div>
+                </div>
+            </div>
+            <div class="col-xl-4 mb-30">
+                <div class="card-box height-100-p pd-20">
+                    <h2 class="h4 mb-20">Lead Target</h2>
+                    <div id="chart6"></div>
+                </div>
+            </div> -->
+    </div>
+    <!-- <div class="card-box mb-30"> -->
+    <!-- <h2 class="h4 pd-20">Best Selling Products</h2>
+            <table class="data-table table nowrap">
+                <thead>
+                    <tr>
+                        <th class="table-plus datatable-nosort">Product</th>
+                        <th>Name</th>
+                        <th>Color</th>
+                        <th>Size</th>
+                        <th>Price</th>
+                        <th>Oty</th>
+                        <th class="datatable-nosort">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="table-plus">
+                            <img src="<?= base_url() ?>deskapp-master/vendors/images/product-1.jpg" width="70" height="70" alt="">
+                        </td>
+                        <td>
+                            <h5 class="font-16">Shirt</h5>
+                            by John Doe
+                        </td>
+                        <td>Black</td>
+                        <td>M</td>
+                        <td>$1000</td>
+                        <td>1</td>
+                        <td>
+                            <div class="dropdown">
+                                <a class="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+                                    <i class="dw dw-more"></i>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+                                    <a class="dropdown-item" href="#"><i class="dw dw-eye"></i> View</a>
+                                    <a class="dropdown-item" href="#"><i class="dw dw-edit2"></i> Edit</a>
+                                    <a class="dropdown-item" href="#"><i class="dw dw-delete-3"></i> Delete</a>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="table-plus">
+                            <img src="<?= base_url() ?>deskapp-master/vendors/images/product-2.jpg" width="70" height="70" alt="">
+                        </td>
+                        <td>
+                            <h5 class="font-16">Boots</h5>
+                            by Lea R. Frith
+                        </td>
+                        <td>brown</td>
+                        <td>9UK</td>
+                        <td>$900</td>
+                        <td>1</td>
+                        <td>
+                            <div class="dropdown">
+                                <a class="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+                                    <i class="dw dw-more"></i>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+                                    <a class="dropdown-item" href="#"><i class="dw dw-eye"></i> View</a>
+                                    <a class="dropdown-item" href="#"><i class="dw dw-edit2"></i> Edit</a>
+                                    <a class="dropdown-item" href="#"><i class="dw dw-delete-3"></i> Delete</a>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="table-plus">
+                            <img src="<?= base_url() ?>deskapp-master/vendors/images/product-3.jpg" width="70" height="70" alt="">
+                        </td>
+                        <td>
+                            <h5 class="font-16">Hat</h5>
+                            by Erik L. Richards
+                        </td>
+                        <td>Orange</td>
+                        <td>M</td>
+                        <td>$100</td>
+                        <td>4</td>
+                        <td>
+                            <div class="dropdown">
+                                <a class="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+                                    <i class="dw dw-more"></i>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+                                    <a class="dropdown-item" href="#"><i class="dw dw-eye"></i> View</a>
+                                    <a class="dropdown-item" href="#"><i class="dw dw-edit2"></i> Edit</a>
+                                    <a class="dropdown-item" href="#"><i class="dw dw-delete-3"></i> Delete</a>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="table-plus">
+                            <img src="<?= base_url() ?>deskapp-master/vendors/images/product-4.jpg" width="70" height="70" alt="">
+                        </td>
+                        <td>
+                            <h5 class="font-16">Long Dress</h5>
+                            by Renee I. Hansen
+                        </td>
+                        <td>Gray</td>
+                        <td>L</td>
+                        <td>$1000</td>
+                        <td>1</td>
+                        <td>
+                            <div class="dropdown">
+                                <a class="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+                                    <i class="dw dw-more"></i>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+                                    <a class="dropdown-item" href="#"><i class="dw dw-eye"></i> View</a>
+                                    <a class="dropdown-item" href="#"><i class="dw dw-edit2"></i> Edit</a>
+                                    <a class="dropdown-item" href="#"><i class="dw dw-delete-3"></i> Delete</a>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="table-plus">
+                            <img src="<?= base_url() ?>deskapp-master/vendors/images/product-5.jpg" width="70" height="70" alt="">
+                        </td>
+                        <td>
+                            <h5 class="font-16">Blazer</h5>
+                            by Vicki M. Coleman
+                        </td>
+                        <td>Blue</td>
+                        <td>M</td>
+                        <td>$1000</td>
+                        <td>1</td>
+                        <td>
+                            <div class="dropdown">
+                                <a class="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+                                    <i class="dw dw-more"></i>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+                                    <a class="dropdown-item" href="#"><i class="dw dw-eye"></i> View</a>
+                                    <a class="dropdown-item" href="#"><i class="dw dw-edit2"></i> Edit</a>
+                                    <a class="dropdown-item" href="#"><i class="dw dw-delete-3"></i> Delete</a>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table> -->
+
+    <!-- <div class="main-container">
+        <div class="pd-ltr-20">
+            <div class="min-height-200px">
+                <div class="bg-white pd-20 card-box mb-30">
+                    <canvas id="myChart" height="100" style="height: 100px;"></canvas>
+                </div>
+                <div class="bg-white pd-20 card-box mb-30">
+                    <canvas id="myChart" height="100" style="height: 100px;"></canvas>
+                </div>
+            </div>
+        </div>
+    </div> -->
+    <div class="main-container">
+        <div class="pd-ltr-20 xs-pd-20-10">
+            <div class="min-height-200px">
+                <div class="page-header">
+                    <div class="row">
+                        <div class="col-md-6 mb-30">
+                            <div class="pd-20 card-box height-100-p">
+                                <h4 class="h4 text-blue">Produk</h4>
+                                <!-- <div id="myChart"></div> -->
+                                <canvas id="myChart" height="100" style="height: 100px;"></canvas>
+                            </div>
+                        </div>
+                        <div class="col-md-6 mb-30">
+                            <div class="pd-20 card-box height-100-p">
+                                <h4 class="h4 text-blue">Pelanggan</h4>
+                                <!-- <div id="myChartsa"></div> -->
+                                <canvas id="myChartsa" height="100" style="height: 100px;"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
